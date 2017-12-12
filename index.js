92ee54e4979eaf0c121ed8e22417ed3abcf6a233
Calista Nasser <nasser.calista@gmail.com>
2017-12-12 19:51:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 function happyHolidays() {
   return "Happy holidays!";
 }
-function happyHolidaysTo() {
+function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!";
 }
-function happyHolidayTo() {
+function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!";
 }
 function holidayCountdown(holiday, days) {
